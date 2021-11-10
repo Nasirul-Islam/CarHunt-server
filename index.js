aae1c1b8ad071edbf1d032a8c62b113a7653fb19
Nasirul-Islam <nasirulislam866@gmail.com>
2021-11-10 23:39:35 +0600

    post & get method added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-var cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,11 +12,43 @@ app.use(express.json());
 // username: process.env.DB_USER
 // password: process.env.DB_PASS
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.la6rz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("carhunt");
+    const productsCollection = database.collection("products");
+    const ordersCollection = database.collection("orders");
+    const usersCollection = database.collection("users");
+
+    // POST API
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      res.json(result);
+    });
+    // GET API
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find({}).toArray();
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Running server at http://localhost:${port}`)
-})
+  console.log(`Running server at http://localhost:${port}`);
+});
